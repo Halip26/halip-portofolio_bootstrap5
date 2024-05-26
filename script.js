984eb9ccd209805de4ab8cd741f803505a584287
Halip26 <halipuddin.angko@gmail.com>
2024-05-26 22:42:00 +0800

    add currentYear

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxnGTEZfx_kSK1dzujpHBsMQyjF22cMdGUIIjfmtNBPpuXs2kZiWgZ5KkVtxVqQxDFXwA/exec';
-      const form = document.forms['halip26-contact-me'];
-      const btnKirim = document.querySelector('.btn-kirim');
-      const btnLoading = document.querySelector('.btn-loading');
-      const myAlert = document.querySelector('.my-alert');
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxnGTEZfx_kSK1dzujpHBsMQyjF22cMdGUIIjfmtNBPpuXs2kZiWgZ5KkVtxVqQxDFXwA/exec";
+const form = document.forms["halip26-contact-me"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   // ketika tombol submit diklik
   // tampilkan tombol loading, hilangkan tombol kirim
-  btnLoading.classList.toggle('d-none');
-  btnKirim.classList.toggle('d-none');
-  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
     .then((response) => {
       // tampilkan tombol kirim, hilangkan tombol loading
-      btnLoading.classList.toggle('d-none');
-      btnKirim.classList.toggle('d-none');
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
       // tampilkan alert
-      myAlert.classList.toggle('d-none');
+      myAlert.classList.toggle("d-none");
       // reset formnya
       form.reset();
-      console.log('Success!', response);
+      console.log("Success!", response);
     })
-    .catch((error) => console.error('Error!', error.message));
+    .catch((error) => console.error("Error!", error.message));
 });
+
+// Get the current year
+const currentYear = new Date().getFullYear();
+// Set the current year to the span with id "currentYear"
+document.getElementById("currentYear").textContent = currentYear;
